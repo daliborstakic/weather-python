3efa0cfcd821d1afb7510f35755ccab90ba73e49
daliborstakic <stakicdalibor64@gmail.com>
2020-09-30 09:54:34 +0200

    Add method temperature
    The method returns a dictionary containing temperatures, pressure and air moisture. Easier with a dictionary than with a list because of keys.

--- weather/weather.py
+++ weather/weather.py
@@ -15,8 +15,12 @@ class Weather():
         """ Returns weather type, e.g. Clouds, Clear """
         return self.data['weather'][0]['main']
 
+    def temperature(self):
+        """ Returns a list containing different temperatures and humidity """
+        return self.data['main']
+
 if __name__ == '__main__':
     url = "http://api.openweathermap.org/data/2.5/weather?id=792078&units=metric&appid=07b8e95750270c86b32d751ea6719ca6"
     wapp = Weather(url)
 
-    print(wapp.get_weather())
+    print(wapp.temperature())
